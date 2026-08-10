ae1b1a49452459b2f216bd6c8142e150e7fa21d8
Vishal Sonwane <vishalsonwane951@gmail.com>
2026-08-10 14:36:33 +0530

    fix: cors

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ const PORT = process.env.PORT || 5000;
 
 // ✅ CORS
 app.use(cors({
-  origin: ["https://my-travel-app-frontend-btiz.vercel.app",
+  origin: ["https://my-travel-app-frontend-3qfb.vercel.app",
     "http://localhost:5173",
     "http://localhost:5174"],
   credentials: true,
